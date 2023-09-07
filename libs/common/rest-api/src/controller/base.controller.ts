3fbefd219fb4a489f2ced3798d1a453cbeddbec4
vduzh <vduzh@tut.by>
2023-09-07 16:18:14 +0300

    Cleaned up

--- libs/common/rest-api/src/controller/base.controller.ts
+++ libs/common/rest-api/src/controller/base.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Param } from '@nestjs/common';
+import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BaseModel, BaseService } from '@app/common/core';
 import { BaseDTO } from './dto/base.dto';
 
@@ -21,5 +21,24 @@ export abstract class BaseController<
   @Get(':id')
   async getById(@Param('id') id: ID): Promise<D> {
     return this.toDTO(await this.service.findById(id));
+  }
+
+  @Post()
+  async add(@Body() dto: D): Promise<D> {
+    const model = await this.service.save(this.toModel(dto));
+    return this.toDTO(await this.service.save(model));
+  }
+
+  @Put(':id')
+  async update(@Body() dto: D, @Param('id') id: ID): Promise<D> {
+    const d = { ...dto, id };
+    return this.toDTO(await this.service.save(this.toModel(d)));
+  }
+
+  //TODOD: implement patch
+
+  @Delete(':id')
+  async deleteAuthor(@Param('id') id: ID): Promise<boolean> {
+    return await this.service.delete(id);
   }
 }
